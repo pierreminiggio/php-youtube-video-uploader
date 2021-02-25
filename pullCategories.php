16dede0c3dd71ea3adfcb5437bf8e8a01e3dc56f
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-25 18:04:50 +0100

    get items

--- pullCategories.php
+++ pullCategories.php
@@ -34,5 +34,29 @@ $refreshToken = $argv[4];
 require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $provider = new AccessTokenProvider();
-
 $accessToken = $provider->get($clientId, $clientSecret, $refreshToken);
+
+$curl = curl_init('https://www.googleapis.com/youtube/v3/videoCategories?regionCode=' . $locale);
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => ['Content-Type: application/json' , 'Authorization: Bearer ' . $accessToken]
+]);
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+if (empty($response)) {
+    die('Error: No response');
+}
+
+$jsonResponse = json_decode($response, true);
+
+if (empty($jsonResponse)) {
+    die('Error: Bad JSON response');
+}
+
+if (empty($jsonResponse['items'])) {
+    die('Error: No "items" key in JSON response');
+}
+
+var_dump($jsonResponse['items']);
